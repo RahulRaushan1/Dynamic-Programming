aabe5b30abce31f1999ff366b955a3d3696b8570
Rahul Raushan <rahulraushan137138@gmail.com>
2025-11-18 12:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (11.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -13,14 +13,26 @@
 //     }
 // }
 
-class Solution { 
-    public int fibo(int n, int[] dp) {
-        if(n<=1) return n;
-        if(dp[n]!=0) return dp[n];
-        return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
-    }
+// class Solution { 
+//     public int fibo(int n, int[] dp) {
+//         if(n<=1) return n;
+//         if(dp[n]!=0) return dp[n];
+//         return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
+//     }
+//     public int fib(int n) {
+//         int[] dp = new int[n+1];  // indox from 0 to n 
+//         return fibo(n,dp);
+//     }
+// }
+
+class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];  // indox from 0 to n 
-        return fibo(n,dp);
+        if(n<=1) return n;
+        int[] dp = new int[n+1];
+        dp[0] =0; dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
