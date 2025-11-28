5e792596a79f7bf7af61cd198ed1cd7e4cab7820
Rahul Raushan <rahulraushan137138@gmail.com>
2025-11-28 12:11:05 +0530

    Time: 10 ms (54.85%) | Memory: 46.8 MB (12.42%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,37 @@
+// class Solution {
+//     public int ways(int i,int[]arr,int target){
+//         if(i==arr.length){
+//             if(target==0) return 1;
+//             else return 0;
+//         }
+//         int add = ways(i+1,arr,target-arr[i]);
+//         int sub = ways(i+1,arr,target+ arr[i]);
+//         return add + sub;
+//     }
+//     public int findTargetSumWays(int[] arr, int target) {
+//         return ways(0,arr,target);
+//     }
+// }
 class Solution {
-    public int ways(int i,int[]arr,int target){
+    static int sum;
+    public int ways(int i,int[]arr,int res,int target, int[][] dp){
         if(i==arr.length){
-            if(target==0) return 1;
+            if(res==target) return 1;  // 1 valid way
             else return 0;
         }
-        int add = ways(i+1,arr,target-arr[i]);
-        int sub = ways(i+1,arr,target+ arr[i]);
-        return add + sub;
+        if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int add = ways(i+1,arr,res+arr[i],target,dp);
+        int sub = ways(i+1,arr,res-arr[i], target,dp);
+        return dp[i][res+sum]= add + sub;
     }
     public int findTargetSumWays(int[] arr, int target) {
-        return ways(0,arr,target);
+        // i --> 0 to n-1 | res = -sum to sum 
+        // dp[i][res+sum]  --> ways(i,res)
+        sum=0; int n = arr.length;
+        for(int ele : arr) sum += ele;
+        int[][] dp = new int [n][2*sum+1];
+        for(int i=0;i<dp.length;i++)
+           for(int j=0;j<dp[0].length;j++) dp[i][j]=-1;
+        return ways(0,arr,0,target,dp);
     }
 }
