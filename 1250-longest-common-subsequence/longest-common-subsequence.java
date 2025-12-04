9f5f2e257cb3ed1011ba619f71377c15191ebcc3
Rahul Raushan <rahulraushan137138@gmail.com>
2025-12-04 10:12:15 +0530

    Time: 34 ms (7.12%) | Memory: 55.8 MB (5.75%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,57 @@
+//               Method 1-----> Recusion (String pass by value)
+// class Solution {
+//     public int longestCommonSubsequence(String a, String b) {
+//         int m = a.length(), n=b.length();
+//         if(m==0 || n==0) return 0;
+//         String a2 = a.substring(0,m-1);  // does not include m-1 idx
+//         String b2 = b.substring(0,n-1);  // does not include n-1 idx
+//         if(a.charAt(m-1)==b.charAt(n-1))
+//            return  1 + longestCommonSubsequence(a2,b2);
+//         else {
+//             return Math.max(longestCommonSubsequence(a,b2),longestCommonSubsequence(a2,b));
+//         }   
+
+//     }
+// }
+
+//             Mehtod 2 ---> Pass by reference
+
+// class Solution {
+//     public int lcs(int i, int j, StringBuilder a, StringBuilder b) {
+//         if(i<0 || j<0) return 0;
+//         if(a.charAt(i) == b.charAt(j))
+//             return 1 + lcs (i-1,j-1,a,b);
+//         else{
+//             return Math.max(lcs(i-1,j,a,b),lcs(i,j-1,a,b));
+//         }    
+//     }
+//     public int longestCommonSubsequence(String text1,String text2){
+//         StringBuilder a = new StringBuilder(text1);
+//         StringBuilder b = new StringBuilder(text2);
+//         int m= a.length(), n = b.length();
+//         return lcs (m-1,n-1,a,b);
+//     }
+// }
+
+class Solution {
+    static int[][] dp;
+    public int lcs(int i, int j, StringBuilder a, StringBuilder b) {
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(a.charAt(i) == b.charAt(j))
+            return dp[i][j] = 1 + lcs (i-1,j-1,a,b);
+        else{
+            return dp[i][j] = Math.max(lcs(i-1,j,a,b),lcs(i,j-1,a,b));
+        }    
+    }
+    public int longestCommonSubsequence(String text1,String text2){
+        StringBuilder a = new StringBuilder(text1);
+        StringBuilder b = new StringBuilder(text2);
+        int m= a.length(), n = b.length();
+        // i = m-1 to 0 | j = n-1 to 0
+        dp = new int [m][n];
+        for(int i=0;i<dp.length;i++)
+          for(int j=0;j<dp[0].length;j++) dp[i][j] =-1;
+        return lcs (m-1,n-1,a,b);
+    }
+}
